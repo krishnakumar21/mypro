b6b80360bd28371665848395ff022baa820f98bf
Gautam Sawhney <skrishnakumar2107@gmail.com>
2015-11-03 16:51:25 +0530

    Added attributes to model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,9 @@ class User < ActiveRecord::Base
   # Autocode: Callback
 
 	# File Upload
+  has_attached_file :pic, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/pic/:style/missing.png"
+  validates_attachment_content_type :pic, content_type: /\Aimage\/.*\Z/ 
+  
 
 	# Soft Destroy
 end
