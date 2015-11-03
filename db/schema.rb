6957ca0bad1db3b2d2f2448a406f61f7a36ef3fc
Gautam Sawhney <skrishnakumar2107@gmail.com>
2015-11-03 17:34:10 +0530

    Updated schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151103114746) do
+ActiveRecord::Schema.define(version: 20151103120346) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,8 @@ ActiveRecord::Schema.define(version: 20151103114746) do
   create_table "patients", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "name"
+    t.string   "pic1"
   end
 
   create_table "posts", force: :cascade do |t|
