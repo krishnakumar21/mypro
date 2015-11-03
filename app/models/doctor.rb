3132baf7142943c54cbbe19083d27c1f31628a8d
Gautam Sawhney <skrishnakumar2107@gmail.com>
2015-11-03 17:17:46 +0530

    Added attributes to model

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -7,6 +7,9 @@ class Doctor < ActiveRecord::Base
   # Autocode: Callback
 
 	# File Upload
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/image/:style/missing.png"
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/ 
+  
 
 	# Soft Destroy
 end
