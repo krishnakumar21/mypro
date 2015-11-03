e7323a2e60a9705732e11541f41d6a67509b92b9
Gautam Sawhney <skrishnakumar2107@gmail.com>
2015-11-03 18:05:29 +0530

    Updated schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151103120626) do
+ActiveRecord::Schema.define(version: 20151103123454) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,8 +19,13 @@ ActiveRecord::Schema.define(version: 20151103120626) do
   create_table "appointments", force: :cascade do |t|
     t.datetime "date"
     t.integer  "count"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+    t.string   "type"
+    t.string   "app_pic_file_name"
+    t.string   "app_pic_content_type"
+    t.integer  "app_pic_file_size"
+    t.datetime "app_pic_updated_at"
   end
 
   create_table "comments", force: :cascade do |t|
