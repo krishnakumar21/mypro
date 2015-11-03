365991c7644f80299e2d93c0af517c508af242c7
Gautam Sawhney <skrishnakumar2107@gmail.com>
2015-11-03 18:30:37 +0530

    Updated schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151103124640) do
+ActiveRecord::Schema.define(version: 20151103125942) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,12 @@ ActiveRecord::Schema.define(version: 20151103124640) do
     t.string   "app_pic_content_type"
     t.integer  "app_pic_file_size"
     t.datetime "app_pic_updated_at"
+  end
+
+  create_table "articles", force: :cascade do |t|
+    t.string   "type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "categories", force: :cascade do |t|
