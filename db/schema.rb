4dfa5ef697aae21deedc63f7414bc6d42e7b2c0f
Gautam Sawhney <skrishnakumar2107@gmail.com>
2015-11-03 17:03:40 +0530

    Updated schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151103112957) do
+ActiveRecord::Schema.define(version: 20151103113309) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,11 @@ ActiveRecord::Schema.define(version: 20151103112957) do
     t.string   "comment_pics_content_type"
     t.integer  "comment_pics_file_size"
     t.datetime "comment_pics_updated_at"
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "posts", force: :cascade do |t|
