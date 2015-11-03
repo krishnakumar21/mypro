61892152b1be5ecdbfde2dac45d4c994f5e366d6
Gautam Sawhney <skrishnakumar2107@gmail.com>
2015-11-03 17:18:03 +0530

    Updated schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151103113908) do
+ActiveRecord::Schema.define(version: 20151103114746) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,8 +36,13 @@ ActiveRecord::Schema.define(version: 20151103113908) do
   create_table "doctors", force: :cascade do |t|
     t.string   "name"
     t.integer  "age"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "address"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "likes", force: :cascade do |t|
