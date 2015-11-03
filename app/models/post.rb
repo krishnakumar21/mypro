2acda5835edeae6ef65832bbedeffd8549d7d819
Gautam Sawhney <skrishnakumar2107@gmail.com>
2015-11-03 16:57:41 +0530

    Added attributes to model

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,6 +8,9 @@ class Post < ActiveRecord::Base
   # Autocode: Callback
 
 	# File Upload
+  has_attached_file :post_pic, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/post_pic/:style/missing.png"
+  validates_attachment_content_type :post_pic, content_type: /\Aimage\/.*\Z/ 
+  
 
 	# Soft Destroy
 end
