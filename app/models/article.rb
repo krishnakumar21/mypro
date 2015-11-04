260e0c32375f4befb0ad0abed0a717651812c77b
Gautam Sawhney <skrishnakumar2107@gmail.com>
2015-11-04 10:34:47 +0530

    Added attributes to model

--- app/models/article.rb
+++ app/models/article.rb
@@ -7,6 +7,9 @@ class Article < ActiveRecord::Base
   # Autocode: Callback
 
 	# File Upload
+  has_attached_file :article_image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/article_image/:style/missing.png"
+  validates_attachment_content_type :article_image, content_type: /\Aimage\/.*\Z/ 
+  
 
 	# Soft Destroy
 end
